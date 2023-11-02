0b0fdb64ecd3ab2cb5799306455339c1c1c04926
André Shiniti Freire Abbe <shiniti67@gmail.com>
2023-11-02 18:33:03 +0000

    starting header design

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ArrowIcon, ArrowIconWhite } from './assets/arrow-icon'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className='header-container'>
+        <div className='logo-container'>
+          <img src="/logo-auto-center.png" />
+          <div className='logo-label'>
+          <span className='logo-text-bold'>Auto Center</span>
+          <span className='logo-text-regular'>Service</span>
+        </div>
+        </div>
+        
+        <nav className='menu-options'>
+          <a>Home</a>
+          <a>Services</a>
+          <a>Location</a>
+          <a>About Us</a>
+        </nav>
+        <button>
+          <div className='btn-face-white'>
+            Contact us <ArrowIcon/>
+          </div>
+          <div className='btn-face-color'>
+            Contact us <ArrowIconWhite/>
+          </div>
+        
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+      </header>
     </>
   )
 }
